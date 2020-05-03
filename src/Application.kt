1d7ac2ae5f409e642355ed1a64ef1eccc4f0f526
powerman23rus@gmail.com <A0102783>
2020-05-03 21:29:31 +0300

    Add heroku config#3.7

--- src/Application.kt
+++ src/Application.kt
@@ -14,46 +14,44 @@ import io.ktor.http.HeaderValue
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import java.lang.Exception
 
-
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 private val clients: HashMap<String, String> = hashMapOf()
 val client = HttpClient(CIO)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun main(args: Array<String>) {
+    val port = Integer.valueOf(System.getenv("PORT"))
+    embeddedServer(Netty, port) {
+        install(Routing) {
+            get("/") {
+                call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            }
+            get("/{name}") {
 
-    install(ContentNegotiation) {
-        gson {
-        }
-    }
+                call.respondText("HELLO WORLD, ${call.parameters["name"]}!", contentType = ContentType.Text.Plain)
+            }
 
-    routing {
-        get("/") {
+            get("/json/gson") {
+                call.respond(mapOf("hello" to "world"))
+            }
 
-            call.respondText("HELLO WORLD, ${call.parameters["name"]}!", contentType = ContentType.Text.Plain)
-        }
+            post("/register/{token}/{name}") {
+                val token = call.parameters["token"].orEmpty()
+                val name = call.parameters["name"].orEmpty()
+                clients[name] = token
+                call.respond(HttpStatusCode.OK)
+            }
 
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
-
-        post("/register/{token}/{name}") {
-            val token = call.parameters["token"].orEmpty()
-            val name = call.parameters["name"].orEmpty()
-            clients[name] = token
-            call.respond(HttpStatusCode.OK)
-        }
-
-        post("/complete/{name}") {
-            val token = clients[call.parameters["name"].orEmpty()] ?: return@post
-            val result = """{
+            post("/complete/{name}") {
+                val token = clients[call.parameters["name"].orEmpty()] ?: return@post
+                val result = """{
                      "to" : "$token",
                      "notification" : {
                      "body" : "Complete",
@@ -63,20 +61,20 @@ fun Application.module(testing: Boolean = false) {
                      }
                     }"""
 
-            try {
-                client.post<Unit>("https://fcm.googleapis.com/fcm/send") {
-                    header("Content-Type", "application/json")
-                    header("Authorization", "key=AAAAVJAf9is:APA91bGwlOKs1mH1Y6aH-uZCbkLzuWSi8KyRrdKz5EctxfIvNjYilleUuShNepCzZYb29B510tIhYZyC0T6ZgjvMmdaAoA1ewrMOIKBiz8Ix4P94LQA6twWNDm-37aBwdixQ1VLTvEUi")
-                    body = result
+                try {
+                    client.post<Unit>("https://fcm.googleapis.com/fcm/send") {
+                        header("Content-Type", "application/json")
+                        header("Authorization", "key=AAAAVJAf9is:APA91bGwlOKs1mH1Y6aH-uZCbkLzuWSi8KyRrdKz5EctxfIvNjYilleUuShNepCzZYb29B510tIhYZyC0T6ZgjvMmdaAoA1ewrMOIKBiz8Ix4P94LQA6twWNDm-37aBwdixQ1VLTvEUi")
+                        body = result
+                    }
+
+                    val test = 0
+                } catch (e : Exception) {
+                    call.respond(HttpStatusCode.InternalServerError)
                 }
 
-                val test = 0
-            } catch (e : Exception) {
-                call.respond(HttpStatusCode.InternalServerError)
+                call.respond(HttpStatusCode.OK)
             }
-
-            call.respond(HttpStatusCode.OK)
         }
-    }
+    }.start(wait = true)
 }
-
